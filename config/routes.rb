90407a112fe30336c901599f0ee5e7c23d84f6a9
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-18 12:35:34 -0500

    generate links resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :links
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   root to: "links#index"
---
+++ db/migrate/20191118173514_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :url
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
