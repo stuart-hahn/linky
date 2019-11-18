0be9e1767c19a686cc79948259ec29ab247c90ef
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-18 12:48:34 -0500

    add controller actions for new and create, add partials for new links and errors

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,39 @@
 class LinksController < ApplicationController
+    # CONVENTION index show new edit create update destroy
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+        @link = Link.new
+    end
+
+    def edit
+    end
+
+    def create
+        @link = current_user.links.build(link_params)
+
+        if @link.save
+            redirect_to link_path(@link), notice: "Link successfully created"
+        else
+            render :new, alert: "Failed to create link"
+        end
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def link_params
+        params.require(:link).permit(:title, :url)
+    end
+
 end
