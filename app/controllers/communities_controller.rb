14820f18c5f645bac76d87460265f54bd9965b96
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-19 14:40:02 -0500

    add CRUD actions to communities controller

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -7,26 +7,39 @@ class CommunitiesController < ApplicationController
     end
 
     def show
-
+        @community = Community.find_by(id: params[:id])
     end
 
     def new
-
+        @community = Community.new
     end
 
     def edit
-
+        @community = Community.find_by(id: params[:id])
     end
 
     def create
+        @communty = Community.new(community_params)
 
+        if @community.save
+            redirect_to community_path(@community), notice: "SUCCESS"
+        else
+            flash.now[:alert] = "FAILURE"
+            render :new
+        end
     end
 
     def update
-
+        @community = Community.find_by(id: params[:id])
     end
 
     def destroy
+        @community = Community.find_by(id: params[:id])
+    end
 
+    private
+
+    def community_params
+        params.require(:community).permit(:title)
     end
 end
