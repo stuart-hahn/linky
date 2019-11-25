9ce0f29f290e2d7455138b354f65b60aec279a2c
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-25 11:06:36 -0500

    remove unused binding.pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,7 @@ class User < ApplicationRecord
     votes.exists?(downvote: 1, link: link)
   end
   
-  def self.from_omniauth(auth)  
-    binding.pry
+  def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
